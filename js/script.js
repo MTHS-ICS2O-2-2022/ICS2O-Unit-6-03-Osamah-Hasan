4f662f0bcf8b3bb42db08dae6903e021eaaf1029
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-05-30 16:58:33 +0000

    add icone change html change web view change css style change js web style

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Osamah Hasan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Osamah Hasan
+// Created on: Feb 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * Check service worker support"
+*/
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register("/ICS2O-Unit-6-01-Osamah-Hasan/sw.js", {
+    scope: "/ICS2O-Unit-6-01-Osamah-Hasan/",
+  })
+}
+
+function myButtonClicked() {
+  alert("Answer will be supported soon")
+}
