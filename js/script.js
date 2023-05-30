0347329f2df3a30105afccc9d17abd5a95d5242d
Osamah_Hasan <124389134+Osamah-Hasan@users.noreply.github.com>
2023-05-30 17:05:07 +0000

    chnage js data add html icone weather

--- js/script.js
+++ js/script.js
@@ -15,6 +15,17 @@ if (navigator.serviceWorker) {
   })
 }
 
-function myButtonClicked() {
-  alert("Answer will be supported soon")
+const getWeather = async (URLAddress) => {
+  try {
+    const result = await fetch(URLAddress)
+    const jsonData = await result.json()
+    jsonData.main.temp = Math.round(jsonData.main.temp - 273.15)
+    jsonData.main.feels_like = Math.round(jsonData.main.feels_like - 273.15)
+    document.getElementById("answer").innerHTML = "The weather is " + jsonData.weather[0].main + " with " + jsonData.weather[0].description + "<br /> <br />" + "The temperature right now is " + jsonData.main.temp + " feels like "+ jsonData.main.feels_like
+    document.getElementById("weather-icon").innerHTML = "<img src='http://openweathermap.org/img/wn/" + jsonData.weather[0].icon + "@2x.png'>"
+  } catch (err) {
+    console.log(err)
+  }
 }
+
+getWeather("https://api.openweathermap.org/data/2.5/weather?lat=45.4211435&lon=-75.6900574&appid=fe1d80e1e103cff8c6afd190cad23fa5")
